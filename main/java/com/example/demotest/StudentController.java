b76c2128754a573ecc5072ae857aebbc5aba6d32
Jonathan <jonathanwnissen@gmail.com>
2018-09-11 12:56:18 +0200

    Jonathan

--- main/java/com/example/demotest/StudentController.java
+++ main/java/com/example/demotest/StudentController.java
@@ -22,6 +22,7 @@ import java.util.logging.Logger;
         students.add(new Student(4, "Jesper", "Nordentoft", "jesper@kea.dk"));
         students.add(new Student(4, "Thomas", "Nordentoft", "jesper@kea.dk"));
         students.add(new Student(4, "Jonathan", "Nordentoft", "jesper@kea.dk"));
+        students.add(new Student(4, "Anders", "Nordentoft", "jesper@kea.dk"));
 
 
         }
